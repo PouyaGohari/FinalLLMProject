8950c7a29c23fe4ce7ee2b6d94bd6e28d8dbb7b9
PouyaGohari <pouya.hmgohari@gmail.com>
2025-07-22 15:17:57 +0330

    Change the parser

--- MyArgParser.py
+++ MyArgParser.py
@@ -1,4 +1,5 @@
 import argparse
 def downloading_adapters():
     parser = argparse.ArgumentParser(description="Download expert adapters from Hugging Face.")
-    parser.add_argument("--hf_token", type=str)
+    parser.add_argument("--hf_token", required=True,  type=str)
+    return parser.parse_args()
