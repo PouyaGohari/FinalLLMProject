63f43db5138cea9699635737a70d5d36f79da1cb
PouyaGohari <pouya.hmgohari@gmail.com>
2025-07-25 17:45:01 +0330

    Parser is altered to use my_cka.py functions

--- MyArgParser.py
+++ MyArgParser.py
@@ -1,5 +1,14 @@
 import argparse
-def downloading_adapters():
-    parser = argparse.ArgumentParser(description="Download expert adapters from Hugging Face.")
+
+def arg_parser():
+    parser = argparse.ArgumentParser()
     parser.add_argument("--hf_token", required=True,  type=str)
+    parser.add_argument("--seed", type=int, default=42)
+    parser = argparse.ArgumentParser(description="Applying arrow router and general knowledge subtraction. Then apply CKA on it.")
+    parser.add_argument("--dataset_path", default=None, type=str)
+    parser.add_argument("--top_k", default=3, type=int)
+    parser.add_argument("--temperature", default=1.0, type=float)
+    parser.add_argument("--gks", default=False, type=bool)
+    parser.add_argument("--base_mode_name", type=str, default="microsoft/Phi-3-mini-4k-instruct", required=True)
+    parser.add_argument("--n_samples", type=int, default=64)
     return parser.parse_args()
