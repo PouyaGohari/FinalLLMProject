5747f2ab2f63219f0fb06848a3dc4b61f98a2c33
PouyaGohari <pouya.hmgohari@gmail.com>
2025-07-26 14:00:35 +0330

    Change gks nature.

--- MyArgParser.py
+++ MyArgParser.py
@@ -7,7 +7,7 @@ def arg_parser():
     parser.add_argument("--dataset_path", default=None, type=str)
     parser.add_argument("--top_k", default=3, type=int)
     parser.add_argument("--temperature", default=1.0, type=float)
-    parser.add_argument("--gks", default=False, type=bool)
+    parser.add_argument("--gks", action='store_true', help="Use general knowledge subtraction")
     parser.add_argument("--base_model_name", type=str, default="microsoft/Phi-3-mini-4k-instruct", required=True)
     parser.add_argument("--n_samples", type=int, default=64)
     return parser.parse_args()
