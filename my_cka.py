c161b50774116ea2a31f312d7a8af689124cd7d2
PouyaGohari <pouya.hmgohari@gmail.com>
2025-07-26 13:31:18 +0330

    There was an mistake on the apply_arrow_gks function that was corrected.

--- my_cka.py
+++ my_cka.py
@@ -83,7 +83,7 @@ def apply_arrow_or_gks(
             arrow_top_k=arrow_top_k,  # Number of selected task-specific LoRAs for each token in each layer
             arrow_router_temperature=arrow_router_temperature,
             use_gks=gks,  # ← enable GenKnowSub!
-            le_names=list(language_experts.keys()),  # name of loaded general-domain LoRAs
+            ts_names=list(cluster_names.keys()),  # name of loaded general-domain LoRAs
             target_modules=target_modules
         )
     model.add_adapter(adapter_name="router", peft_config=arrow_config)
