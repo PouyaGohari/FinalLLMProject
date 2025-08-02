a162f88ac4da14ea75d6cff14ddeb7c0f0edeb80
PouyaGohari <pouya.hmgohari@gmail.com>
2025-08-02 19:49:52 +0330

    Change the main.py to print for debugging purposes.

--- main.py
+++ main.py
@@ -116,8 +116,10 @@ if __name__=='__main__':
         second_model_name = "GenKowlSub"
     else:
         second_model_name = "Arrow"
-
     layers_of_interest = [f"model.layers.{i}.self_attn.o_proj.weight" for i in range(31)] + [f"model.layers.{i}.self_attn.qkv_proj.weight" for i in range(31)]
+    for name, layer in general_model.named_modules():
+        if layer in layers_of_interest:
+            print("yes")
     exported_dat = apply_cka(
         first_loader=my_dataloader,
         base_model=general_model,
