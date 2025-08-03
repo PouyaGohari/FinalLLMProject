c7bf7e03b8cfe64b401606bb0536a9412ebf0495
PouyaGohari <pouya.hmgohari@gmail.com>
2025-08-03 18:54:58 +0330

    asd

--- MyConfig.py
+++ MyConfig.py
@@ -5,5 +5,4 @@ CLUSTER_NAMES = {f'cluster{i}':f"TahaBa/phi3-mini-clustered-flan/ts_expert_{i}" 
 # Since we are using a single expert in English in our directory is a single dictionary. Change it with your configurations.
 LANGUAGE_EXPERTS = {'English_adapter': 'TahaBa/phi3-mini-general-adapters/cluster0_batch16_prop1.0_langen/checkpoint-16'}
 DATA_FILE = {'train': 'en_Wiki_10k_LM_511_1 (1).json', 'test': 'en_Wiki_10k_LM_511_1_test (1).json'}
-TARGET_MODULES = ["qkv_proj", "o_proj"]
-MAX_LENGTH=1024
+MAX_LENGTH = 1024
