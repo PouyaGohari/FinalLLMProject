02d5be87e49d6ae56cba7c9e359b7cd7539f1fe1
PouyaGohari <pouya.hmgohari@gmail.com>
2025-08-10 20:04:17 +0330

    Modify the previous custom dataset to avoid create another renundant custom dataset.

--- custom_dataset.py
+++ custom_dataset.py
@@ -3,12 +3,13 @@ from transformers import AutoTokenizer
 import datasets
 from MyConfig import *
 
-class CustomDatasetCKA(Dataset):
-    def __init__(self, text_dataset:datasets, tokenizer:AutoTokenizer.from_pretrained):
+class CustomDataset(Dataset):
+    def __init__(self, text_dataset:datasets, tokenizer:AutoTokenizer.from_pretrained, use_attention:bool=False):
         super().__init__()
         self.tokenizer = tokenizer
         self.dataset = text_dataset
         self.tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+        self.use_attention = use_attention
 
     def __len__(self):
         return len(self.dataset)
@@ -26,5 +27,8 @@ class CustomDatasetCKA(Dataset):
             tokenize=True,
             max_length=MAX_LENGTH,
             return_tensors="pt",
+            return_dict=self.use_attention,
         )
-        return {'input_ids': text.squeeze(0).to('cuda')}
+        if self.use_attention:
+            return {'input_ids': text['input_ids'].squeeze(0).to('cuda'), 'attention_mask': text['attention_mask'].squeeze(0).to('cuda')}
+        return {'input_ids':text.squeeze(0).to('cuda')}
