92659dfbb8303342ad8fa5a83577eb3cfa5e0307
PouyaGohari <pouya.hmgohari@gmail.com>
2025-08-10 16:47:20 +0330

    Change the apply_cka function for adapting to new CKA repo

--- my_cka.py
+++ my_cka.py
@@ -1,5 +1,6 @@
-from torch_cka import CKA
+from ckatorch import CKA
 import torch
+
 from torch.utils.data import DataLoader
 import datasets
 from custom_dataset import CustomDataset
@@ -20,95 +21,88 @@ from typing import (
     Optional
 )
 
-class CustomCKA(CKA):
-    def __init__(self,
-        base_model:AutoModelForCausalLM,
-        enhanced_model:PeftModel,
-        first_model_name:str,
-        second_model_name:str,
-        base_model_layers: Optional[List[str]] = None,
-        enhanced_model_layers: Optional[List[str]] = None,
-        device:str='cuda'):
-        super().__init__(
-            model1=base_model,
-            model2=enhanced_model,
-            model1_name=first_model_name,
-            model2_name=second_model_name,
-            model1_layers=base_model_layers,
-            model2_layers=enhanced_model_layers,
-            device=device
-        )
+# class CustomCKA(CKA):
+#     def __init__(self,
+#         base_model:AutoModelForCausalLM,
+#         enhanced_model:PeftModel,
+#         first_model_name:str,
+#         second_model_name:str,
+#         base_model_layers: Optional[List[str]] = None,
+#         enhanced_model_layers: Optional[List[str]] = None,
+#         device:str='cuda'):
+#         super().__init__(
+#             model1=base_model,
+#             model2=enhanced_model,
+#             model1_name=first_model_name,
+#             model2_name=second_model_name,
+#             model1_layers=base_model_layers,
+#             model2_layers=enhanced_model_layers,
+#             device=device
+#         )
 
-    def _HSIC(self, K, L):
-        """
-        Computes the unbiased estimate of HSIC metric.
+#     def _HSIC(self, K, L):
+#         """
+#         Computes the unbiased estimate of HSIC metric.
 
-        Reference: https://arxiv.org/pdf/2010.15327.pdf Eq (3)
-        """
-        K = K.to(torch.float32)
-        L = L.to(torch.float32)
+#         Reference: https://arxiv.org/pdf/2010.15327.pdf Eq (3)
+#         """
 
-        N = K.shape[0]
-        ones = torch.ones(N, 1, dtype=K.dtype, device=self.device)
-        result = torch.trace(K @ L)
-        result += ((ones.t() @ K @ ones @ ones.t() @ L @ ones) / ((N - 1) * (N - 2))).item()
-        result -= ((ones.t() @ K @ L @ ones) * 2 / (N - 2)).item()
-        print(result)
-        return (1 / (N * (N - 3)) * result).item()
+#         N = K.shape[0]
+#         ones = torch.ones(N, 1, dtype=K.dtype, device=self.device)
+#         result = torch.trace(K @ L)
+#         result += ((ones.t() @ K @ ones @ ones.t() @ L @ ones) / ((N - 1) * (N - 2))).item()
+#         result -= ((ones.t() @ K @ L @ ones) * 2 / (N - 2)).item()
+#         return (1 / (N * (N - 3)) * result).item()
 
-    def compare(self,
-                dataloader1: DataLoader,
-                dataloader2: DataLoader = None) -> None:
-        """
-        Computes the feature similarity between the models on the
-        given datasets.
-        :param dataloader1: (DataLoader)
-        :param dataloader2: (DataLoader) If given, model 2 will run on this
-                            dataset. (default = None)
-        """
+#     def compare(self,
+#                 dataloader1: DataLoader,
+#                 dataloader2: DataLoader = None) -> None:
+#         """
+#         Computes the feature similarity between the models on the
+#         given datasets.
+#         :param dataloader1: (DataLoader)
+#         :param dataloader2: (DataLoader) If given, model 2 will run on this
+#                             dataset. (default = None)
+#         """
 
-        if dataloader2 is None:
-            warn("Dataloader for Model 2 is not given. Using the same dataloader for both models.")
-            dataloader2 = dataloader1
+#         if dataloader2 is None:
+#             warn("Dataloader for Model 2 is not given. Using the same dataloader for both models.")
+#             dataloader2 = dataloader1
 
-        self.model1_info['Dataset'] = dataloader1.dataset.__repr__().split('\n')[0]
-        self.model2_info['Dataset'] = dataloader2.dataset.__repr__().split('\n')[0]
+#         self.model1_info['Dataset'] = dataloader1.dataset.__repr__().split('\n')[0]
+#         self.model2_info['Dataset'] = dataloader2.dataset.__repr__().split('\n')[0]
 
-        N = len(self.model1_layers) if self.model1_layers is not None else len(list(self.model1.modules()))
-        M = len(self.model2_layers) if self.model2_layers is not None else len(list(self.model2.modules()))
-        print(f'N = {N}, M = {M}')
+#         N = len(self.model1_layers) if self.model1_layers is not None else len(list(self.model1.modules()))
+#         M = len(self.model2_layers) if self.model2_layers is not None else len(list(self.model2.modules()))
 
-        self.hsic_matrix = torch.zeros(N, M, 3)
+#         self.hsic_matrix = torch.zeros(N, M, 3)
 
-        num_batches = min(len(dataloader1), len(dataloader1))
+#         num_batches = min(len(dataloader1), len(dataloader1))
+#         eps = 1e-8
+#         for x1, x2 in tqdm(zip(dataloader1, dataloader2), desc="| Comparing features |", total=num_batches):
+#             self.model1_features = {}
+#             self.model2_features = {}
+#             _ = self.model1(x1.squeeze(1).to(self.device))
+#             _ = self.model2(x2.squeeze(1).to(self.device))
 
-        for x1, x2 in tqdm(zip(dataloader1, dataloader2), desc="| Comparing features |", total=num_batches):
-            self.model1_features = {}
-            self.model2_features = {}
-            _ = self.model1(x1.squeeze(1).to(self.device))
-            _ = self.model2(x2.squeeze(1).to(self.device))
+#             for i, (name1, feat1) in enumerate(self.model1_features.items()):
+#                 X = feat1.flatten(1).to(torch.float32)
+#                 K = X @ X.t()
+#                 K.fill_diagonal_(0.0)
+#                 self.hsic_matrix[i, :, 0] += self._HSIC(K, K) / num_batches
+#                 for j, (name2, feat2) in enumerate(self.model2_features.items()):
+#                     Y = feat2.flatten(1).to(torch.float32)
+#                     L = Y @ Y.t()
+#                     L.fill_diagonal_(0)
+#                     assert K.shape == L.shape, f"Feature shape mismatch! {K.shape}, {L.shape}"
 
-            for i, (name1, feat1) in enumerate(self.model1_features.items()):
-                X = feat1.flatten(1)
-                K = X @ X.t()
-                K.fill_diagonal_(0.0)
-                self.hsic_matrix[i, :, 0] += self._HSIC(K, K) / num_batches
-
-                for j, (name2, feat2) in enumerate(self.model2_features.items()):
-                    Y = feat2.flatten(1)
-                    print(Y)
-                    L = Y @ Y.t()
-                    L.fill_diagonal_(0)
-                    assert K.shape == L.shape, f"Feature shape mismatch! {K.shape}, {L.shape}"
-
-                    self.hsic_matrix[i, j, 1] += self._HSIC(K, L) / num_batches
-                    self.hsic_matrix[i, j, 2] += self._HSIC(L, L) / num_batches
-                print(self.hsic_matrix)
-            del x1, x2, _, self.model1_features, self.model2_features
-        print(self.hsic_matrix)
-        self.hsic_matrix = self.hsic_matrix[:, :, 1] / (self.hsic_matrix[:, :, 0].sqrt() *
-                                                        self.hsic_matrix[:, :, 2].sqrt())
-        assert not torch.isnan(self.hsic_matrix).any(), "HSIC computation resulted in NANs"
+#                     self.hsic_matrix[i, j, 1] += self._HSIC(K, L) / num_batches
+#                     self.hsic_matrix[i, j, 2] += self._HSIC(L, L) / num_batches
+#             del x1, x2, _
+#         self.hsic_matrix = self.hsic_matrix[:, :, 1] / ((self.hsic_matrix[:, :, 0].sqrt() + 1e-8) *
+#                                                         (self.hsic_matrix[:, :, 2].sqrt() + 1e-8))
+#         self.hsic_matrix = self.hsic_matrix.detach().cpu() 
+#         assert not torch.isnan(self.hsic_matrix).any(), "HSIC computation resulted in NANs"
 
 def apply_arrow_or_gks(
         base_model_name:str,
@@ -206,7 +200,7 @@ def apply_cka(
         enhanced_model_layers: Optional[List[str]] = None,
         second_loader: Optional[DataLoader] = None,
         device:str='cuda',
-    ) -> Optional[Dict]:
+    ) -> torch.Tensor:
     """
     This function will compare two given models and two different dataset as you wish just like documentation of the torch_cka library.
     :param first_loader: The data loader for first dataset.
@@ -223,23 +217,14 @@ def apply_cka(
     :return:
     If export_data has been set to true it would return a dictionary contain the data after comparing two models otherwise None would be returned.
     """
-    cka = CustomCKA(
-        base_model,
-        enhanced_model,
-        first_model_name,
-        second_model_name,
-        base_model_layers,
-        enhanced_model_layers,
-        device
+    cka = CKA(
+        first_model=base_model,
+        second_model=enhanced_model,
+        layers=base_model_layers,
+        second_layers=enhanced_model_layers,
+        first_name=first_model_name,
+        second_name=second_model_name,
+        device=device,
     )
-
-    cka.compare(
-        dataloader1=first_loader,
-        dataloader2=second_loader,
-    )
-
-    if show_plot:
-        cka.plot_results()
-    if export_data:
-        return cka.export()
-    return None
+    cka_matrix_different = cka(first_loader, epochs=1)
+    return cka_matrix_different
